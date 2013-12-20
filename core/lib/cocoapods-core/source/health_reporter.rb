CocoaPods/Core

[Source] Add support for data providers

lib/cocoapods-core/source/health_reporter.rb
@@ -124,7 +124,7 @@ module Pod
       # @return [void]
       #
       def check_stray_specs
-        all_paths = Pathname.glob(source.repo + '**/*.podspec{,.yaml}')
+        all_paths = Pathname.glob(source.data_provider.repo + '**/*.podspec{,.yaml}')
         stray_specs = all_paths - report.analyzed_paths
         stray_specs.each do |path|
           report.add_message(:error, "Stray spec", path)

amorde/CocoaPods

core/lib/cocoapods-core/source/health_reporter.rb
@@ -124,7 +124,7 @@ module Pod
       # @return [void]
       #
       def check_stray_specs
-        all_paths = Pathname.glob(source.repo + '**/*.podspec{,.json}')
+        all_paths = Pathname.glob(source.data_provider.repo + '**/*.podspec{,.json}')
         stray_specs = all_paths - report.analyzed_paths
         stray_specs.each do |path|
           report.add_message(:error, "Stray spec", path)