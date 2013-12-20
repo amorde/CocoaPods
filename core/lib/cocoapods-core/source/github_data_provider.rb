CocoaPods/Core

[Source] Add GitHubDataProvider

lib/cocoapods-core/source/github_data_provider.rb
@@ -1,0 +1,142 @@
+module Pod
+  class Source
+
+    # Data provider for a `Pod::Source` backed by a repository hosted on GitHub
+    # and accessed via the HTTP API. Only pure YAML repos using the `Specs`
+    # subdir to store the specifications are supported.
+    #
+    class GitHubDataProvider < AbstractDataProvider
+
+      # @return [String] The identifier of the repository (user name and repo
+      #         name) or the full URL of the repo.
+      #
+      attr_reader :repo_id
+
+      # @return [String] The branch of the repo if the default one shouldn't be
+      #         used.
+      #
+      attr_reader :branch
+
+      # @param [String] repo_id @see repo_id
+      # @param [String] branch @see branch
+      #
+      def initialize(repo_id, branch = nil)
+        @repo_id = repo_id
+        @branch = branch
+      end
+
+      public
+
+      # @group Data Source
+      #-----------------------------------------------------------------------#
+
+      # @return [String] The name of the Source. User name and repo.
+      #
+      def name
+        GitHub.normalized_repo_id(repo_id)
+      end
+
+      # @return [String] The user friendly type of the source.
+      #
+      def type
+        "GitHub API"
+      end
+
+      # @return [Array<String>] The list of the name of all the Pods known to
+      #         the Source.
+      #
+      def pods
+        root_contents = get_github_contents("Specs")
+        dir_names(root_contents)
+      end
+
+      # @return [Array<String>] All the available versions of a given Pod,
+      #         sorted from highest to lowest.
+      #
+      # @param  [String] name
+      #         The name of the Pod.
+      #
+      def versions(name)
+        raise ArgumentError, "No name" unless name
+        contents = get_github_contents("Specs/#{name}")
+        dir_names(contents)
+      end
+
+      # @return [Specification] The specification for a given version of a Pod.
+      #
+      # @param  [String] name
+      #         The name of the Pod.
+      #
+      # @param  [String] version
+      #         The version of the Pod.
+      #
+      def specification(name, version)
+        raise ArgumentError, "No name" unless name
+        raise ArgumentError, "No version" unless version
+        spec_content = specification_contents(name, version)
+        if spec_content
+          Pod::Specification.from_yaml(spec_content)
+        end
+      end
+
+      # @return [Specification] The contents of the specification for a given
+      #         version of a Pod.
+      #
+      # @param  [String] name
+      #         the name of the Pod.
+      #
+      # @param  [String] version
+      #         the version of the Pod.
+      #
+      def specification_contents(name, version)
+        raise ArgumentError, "No name" unless name
+        raise ArgumentError, "No version" unless version
+        path = "Specs/#{name}/#{version}/#{name}.podspec.yaml"
+        file_contents = get_github_contents(path)
+        if file_contents
+          if file_contents['encoding'] == 'base64'
+            require "base64"
+            Base64.decode64(file_contents['content'])
+          end
+        end
+      end
+
+      private
+
+      # @group Private Helpers
+      #-----------------------------------------------------------------------#
+
+      # Performs a get request with the given URL.
+      #
+      # @param [String] url
+      #        The URL of the resource.
+      #
+      # @return [Array, Hash] The information of the resource as Ruby objects.
+      #
+      def get_github_contents(path = nil)
+        Pod::GitHub.contents(repo_id, path, branch)
+      end
+
+      # @param  [Array] [Array<Hash>] The contents of a directory.
+      #
+      # @return [Array<String>] Returns the list of the directories given the
+      #         contents returned for the API of a directory.
+      #
+      # @return [Nil] If the directory was not found or the contents is not an
+      #         array.
+      #
+      def dir_names(contents)
+        if contents.is_a?(Array)
+          contents.map do |entry|
+            if entry['type'] == 'dir'
+              entry['name']
+            end
+          end.compact
+        end
+      end
+
+      #-----------------------------------------------------------------------#
+
+    end
+  end
+end

spec/source/github_data_provider_spec.rb
@@ -1,0 +1,145 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+module Pod
+  describe Source::GitHubDataProvider do
+
+    before do
+      @sut = Source::GitHubDataProvider.new('CocoaPods/Specs', 'yaml_podspecs')
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "In general" do
+      it "returns the name of the source" do
+        @sut.name.should == 'CocoaPods/Specs'
+      end
+
+      it "returns the type of the source" do
+        @sut.type.should == "GitHub API"
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#pods" do
+      it "returns the list of all the Pods" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.pods.should.include?('ARAnalytics')
+        end
+      end
+
+      it "only considers directories to compute the name of Pods" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.pods.should.not.include?('Readme.md')
+        end
+      end
+
+      it "returns nil if no Pods could be found" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut = Source::GitHubDataProvider.new('CocoaPods/Missing_Specs')
+          @sut.pods.should.be.nil
+        end
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#versions" do
+      it "returns the available versions of a Pod" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.versions('ARAnalytics').should == ["1.0", "1.1", "1.2", "1.3.1", "1.3"]
+        end
+      end
+
+      it "returns nil the Pod is unknown" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.versions('Unknown_Pod').should.be.nil
+        end
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.versions(nil)
+        end.message.should.match /No name/
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#specification" do
+      it "returns the specification given the name and the version" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification('ARAnalytics', "1.3.1")
+          spec.name.should == 'ARAnalytics'
+          spec.version.to_s.should == '1.3.1'
+        end
+      end
+
+      it "returns nil if the Pod is unknown" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification('Unknown_Pod', "1.3.1")
+          spec.should.be.nil
+        end
+      end
+
+      it "returns nil if the version of the Pod doesn't exists" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification('ARAnalytics', "0.99.0")
+          spec.should.be.nil
+        end
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification(nil, "0.99.0")
+        end.message.should.match /No name/
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification('ARAnalytics', nil)
+        end.message.should.match /No version/
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#specification_contents" do
+      it "returns the specification given the name and the version" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification_contents('ARAnalytics', "1.3.1")
+          spec.should.include("name: ARAnalytics\nversion: 1.3.1")
+        end
+      end
+
+      it "returns nil if the Pod is unknown" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification_contents('Unknown_Pod', "1.3.1")
+          spec.should.be.nil
+        end
+      end
+
+      it "returns nil if the version of the Pod doesn't exists" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification_contents('ARAnalytics', "0.99.0")
+          spec.should.be.nil
+        end
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification_contents(nil, "0.99.0")
+        end.message.should.match /No name/
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification_contents('ARAnalytics', nil)
+        end.message.should.match /No version/
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+  end
+end

amorde/CocoaPods

core/lib/cocoapods-core/source/github_data_provider.rb
@@ -1,0 +1,142 @@
+module Pod
+  class Source
+
+    # Data provider for a `Pod::Source` backed by a repository hosted on GitHub
+    # and accessed via the HTTP API. Only pure JSON repos using the `Specs`
+    # subdir to store the specifications are supported.
+    #
+    class GitHubDataProvider < AbstractDataProvider
+
+      # @return [String] The identifier of the repository (user name and repo
+      #         name) or the full URL of the repo.
+      #
+      attr_reader :repo_id
+
+      # @return [String] The branch of the repo if the default one shouldn't be
+      #         used.
+      #
+      attr_reader :branch
+
+      # @param [String] repo_id @see repo_id
+      # @param [String] branch @see branch
+      #
+      def initialize(repo_id, branch = nil)
+        @repo_id = repo_id
+        @branch = branch
+      end
+
+      public
+
+      # @group Data Source
+      #-----------------------------------------------------------------------#
+
+      # @return [String] The name of the Source. User name and repo.
+      #
+      def name
+        GitHub.normalized_repo_id(repo_id)
+      end
+
+      # @return [String] The user friendly type of the source.
+      #
+      def type
+        "GitHub API"
+      end
+
+      # @return [Array<String>] The list of the name of all the Pods known to
+      #         the Source.
+      #
+      def pods
+        root_contents = get_github_contents("Specs")
+        dir_names(root_contents)
+      end
+
+      # @return [Array<String>] All the available versions of a given Pod,
+      #         sorted from highest to lowest.
+      #
+      # @param  [String] name
+      #         The name of the Pod.
+      #
+      def versions(name)
+        raise ArgumentError, "No name" unless name
+        contents = get_github_contents("Specs/#{name}")
+        dir_names(contents)
+      end
+
+      # @return [Specification] The specification for a given version of a Pod.
+      #
+      # @param  [String] name
+      #         The name of the Pod.
+      #
+      # @param  [String] version
+      #         The version of the Pod.
+      #
+      def specification(name, version)
+        raise ArgumentError, "No name" unless name
+        raise ArgumentError, "No version" unless version
+        spec_content = specification_contents(name, version)
+        if spec_content
+          Pod::Specification.from_json(spec_content)
+        end
+      end
+
+      # @return [Specification] The contents of the specification for a given
+      #         version of a Pod.
+      #
+      # @param  [String] name
+      #         the name of the Pod.
+      #
+      # @param  [String] version
+      #         the version of the Pod.
+      #
+      def specification_contents(name, version)
+        raise ArgumentError, "No name" unless name
+        raise ArgumentError, "No version" unless version
+        path = "Specs/#{name}/#{version}/#{name}.podspec.json"
+        file_contents = get_github_contents(path)
+        if file_contents
+          if file_contents['encoding'] == 'base64'
+            require "base64"
+            Base64.decode64(file_contents['content'])
+          end
+        end
+      end
+
+      private
+
+      # @group Private Helpers
+      #-----------------------------------------------------------------------#
+
+      # Performs a get request with the given URL.
+      #
+      # @param [String] url
+      #        The URL of the resource.
+      #
+      # @return [Array, Hash] The information of the resource as Ruby objects.
+      #
+      def get_github_contents(path = nil)
+        Pod::GitHub.contents(repo_id, path, branch)
+      end
+
+      # @param  [Array] [Array<Hash>] The contents of a directory.
+      #
+      # @return [Array<String>] Returns the list of the directories given the
+      #         contents returned for the API of a directory.
+      #
+      # @return [Nil] If the directory was not found or the contents is not an
+      #         array.
+      #
+      def dir_names(contents)
+        if contents.is_a?(Array)
+          contents.map do |entry|
+            if entry['type'] == 'dir'
+              entry['name']
+            end
+          end.compact
+        end
+      end
+
+      #-----------------------------------------------------------------------#
+
+    end
+  end
+end

core/spec/source/github_data_provider_spec.rb
@@ -1,0 +1,145 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+module Pod
+  describe Source::GitHubDataProvider do
+
+    before do
+      @sut = Source::GitHubDataProvider.new('CocoaPods/Specs', 'json_podspecs')
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "In general" do
+      it "returns the name of the source" do
+        @sut.name.should == 'CocoaPods/Specs'
+      end
+
+      it "returns the type of the source" do
+        @sut.type.should == "GitHub API"
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#pods" do
+      it "returns the list of all the Pods" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.pods.should.include?('ARAnalytics')
+        end
+      end
+
+      it "only considers directories to compute the name of Pods" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.pods.should.not.include?('Readme.md')
+        end
+      end
+
+      it "returns nil if no Pods could be found" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut = Source::GitHubDataProvider.new('CocoaPods/Missing_Specs')
+          @sut.pods.should.be.nil
+        end
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#versions" do
+      it "returns the available versions of a Pod" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.versions('A3GridTableView').should == ["0.0.1"]
+        end
+      end
+
+      it "returns nil the Pod is unknown" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          @sut.versions('Unknown_Pod').should.be.nil
+        end
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.versions(nil)
+        end.message.should.match /No name/
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#specification" do
+      it "returns the specification given the name and the version" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification('ARAnalytics', "1.3.1")
+          spec.name.should == 'ARAnalytics'
+          spec.version.to_s.should == '1.3.1'
+        end
+      end
+
+      it "returns nil if the Pod is unknown" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification('Unknown_Pod', "1.3.1")
+          spec.should.be.nil
+        end
+      end
+
+      it "returns nil if the version of the Pod doesn't exists" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification('ARAnalytics', "0.99.0")
+          spec.should.be.nil
+        end
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification(nil, "0.99.0")
+        end.message.should.match /No name/
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification('ARAnalytics', nil)
+        end.message.should.match /No version/
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+    describe "#specification_contents" do
+      it "returns the specification given the name and the version" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification_contents('ARAnalytics', "1.3.1")
+          spec.should.include("{\n  \"name\": \"ARAnalytics\",\n  \"version\": \"1.3.1\"")
+        end
+      end
+
+      it "returns nil if the Pod is unknown" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification_contents('Unknown_Pod', "1.3.1")
+          spec.should.be.nil
+        end
+      end
+
+      it "returns nil if the version of the Pod doesn't exists" do
+        VCR.use_cassette('GitHubDataProvider', :record => :new_episodes) do
+          spec = @sut.specification_contents('ARAnalytics', "0.99.0")
+          spec.should.be.nil
+        end
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification_contents(nil, "0.99.0")
+        end.message.should.match /No name/
+      end
+
+      it "raises if the name of the Pod is not provided" do
+        should.raise ArgumentError do
+          @sut.specification_contents('ARAnalytics', nil)
+        end.message.should.match /No version/
+      end
+    end
+
+    #-------------------------------------------------------------------------#
+
+  end
+end